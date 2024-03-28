ab3afe074dde7262121420a5bfa1b9453fbe4a57
FATOKUN EMMANUEL <fatokunemmanuel16@gmail.com>
2024-03-28 08:31:38 +0100

    winner

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let out = document.getElementById("out")
 ringTone = new Audio("ASAP.mp3")
-ringTone1 = new Audio("for-when-it-rains-112785.mp3")
+ringTone1 = new Audio("Winner.mp3")
 ringTone1 = new Audio("filtered-928FBE93-D046-4E4D-8F54-E1DD196E299E.mp3")
-ringTone2 = new Audio("Grade/No cast me .mp3")
+ringTone2 = new Audio("No cast me .mp3")
 ringTone3 = new Audio("Looser.mp3")
 
 function check() {
